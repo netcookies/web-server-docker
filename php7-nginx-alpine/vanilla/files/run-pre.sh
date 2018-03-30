4b74658c6ec467c483c2e86cc0937d4def139804
Isulew <netcookies@gmail.com>
2018-03-30 11:55:32 +0800

    fix vanilla bugs

--- php7-nginx-alpine/vanilla/files/run-pre.sh
+++ php7-nginx-alpine/vanilla/files/run-pre.sh
@@ -10,17 +10,20 @@ if [ -f "/usr/html/index.php" ]; then
     exit 0
 fi
 
-if [ ! -z "$VNL_VERSION" ]; then
+if [ -z "$VNL_VERSION" ]; then
     echo 'The following environment variables need to set: VNL_VERSION '
     exit 1
 fi
 
 
 cd /usr/html
+echo "Start downloading..."
 wget -qO- -O tmp.zip  https://open.vanillaforums.com/get/vanilla-core-${VNL_VERSION}.zip && unzip tmp.zip && rm tmp.zip
+echo "Installing..."
 mv vanilla-*/* . && rm -r vanilla-*
 find . -type d -exec chmod 755 {} \;
 find . -type f -exec chmod 644 {} \;
 chmod -R 777 conf/
 chmod -R 777 uploads/
 chmod -R 777 cache/
+echo "Install successful."
