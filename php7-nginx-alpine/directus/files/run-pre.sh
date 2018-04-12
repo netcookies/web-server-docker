8995effb8cce40b9699bdcd8e33037d48feaa0db
Isulew <netcookies@gmail.com>
2018-04-13 01:16:54 +0800

    update directus env

--- php7-nginx-alpine/directus/files/run-pre.sh
+++ php7-nginx-alpine/directus/files/run-pre.sh
@@ -14,4 +14,16 @@ echo "Start downloading..."
 cd /usr/html
 wget -qO- $(curl -s https://api.github.com/repos/directus/directus/releases/latest | grep browser_download_url | cut -d '"' -f 4 | grep tar.gz) | tar xvz
 echo "Installing..."
+
+php bin/directus install:config -h $DB_HOST -n $DB_NAME -u $DB_USER -p $DB_PASS -e $MAIL_USER
+sed -i "s/'transport' => 'mail'/'transport' => 'smtp'/g" api/configuration.php
+LINE=$(($(grep -n "transport" api/configuration.php | cut -f1 -d:) + 1))
+CONF="       'host' => '"${MAIL_URL}
+CONF=${CONF}"',\n        'username' => '"${MAIL_USER}
+CONF=${CONF}"',\n        'password' => '"${MAIL_PASS}
+CONF=${CONF}"',\n        'port' => '"${MAIL_PORT}"',"
+sed -i "${LINE}i\ ${CONF}" api/configuration.php
+php bin/directus install:database
+php bin/directus install:install -e $ADMIN_USER -p $ADMIN_PASS -t $SITE_NAME
+
 echo "Install successful."
