9e9c7423cfac9fd44c28d42abc0131f27063785d
Isulew <netcookies@gmail.com>
2018-06-01 10:40:10 +0800

    Fix denpendencies missing

--- php7-nginx-alpine/directus/files/run-pre.sh
+++ php7-nginx-alpine/directus/files/run-pre.sh
@@ -15,6 +15,11 @@ cd /usr/html
 wget -qO- $(curl -s https://api.github.com/repos/directus/directus/releases/latest | grep browser_download_url | cut -d '"' -f 4 | grep tar.gz) | tar xvz
 echo "Installing..."
 
+echo "Installing dependencies through composer..."
+curl -s https://getcomposer.org/installer | php
+php composer.phar install
+
+echo "Installing core..."
 php bin/directus install:config -h $DB_HOST -n $DB_NAME -u $DB_USER -p $DB_PASS -e $MAIL_USER
 sed -i "s/'transport' => 'mail'/'transport' => 'smtp'/g" api/configuration.php
 LINE=$(($(grep -n "transport" api/configuration.php | cut -f1 -d:) + 1))
