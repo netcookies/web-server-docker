c08c4f8c27410a57f96b38431dabce56b3f0ae3f
Isulew <netcookies@gmail.com>
2018-06-07 00:39:57 +0800

    Update wordpress docker scripts

--- php7-nginx-alpine/wordpress/files/run-pre.sh
+++ php7-nginx-alpine/wordpress/files/run-pre.sh
@@ -6,6 +6,10 @@ if [ ! -d /usr/html ] ; then
   mkdir -p /usr/html
 fi
 
+if [ ! -d /db ] ; then
+  mkdir -p /db
+fi
+
 if [ -f "/usr/html/index.php" ]; then
     exit 0
 fi
@@ -28,6 +32,13 @@ if [ ! -z "$WP_LOCALE" ]; then
     ARG=$ARG"--locale=${WP_LOCALE} "
 fi
 
+if [ -z "$LOCAL_DEV" ]; then
+    echo "The following environment variables hasn't been set: LOCAL_DEV. Fall back to default value: 1"
+    LOCAL_DEV=1
+    WP_DOMAIN=http://localhost:8000
+fi
+
+
 cd /usr/html
 wp-cli core download $ARG
 while [ ! -f "/usr/html/wp-config.php" ]; do
@@ -37,9 +48,14 @@ done
 
 RESULT=1
 while [ $RESULT -ne 0 ]; do
-    RESULT=`wp-cli core install --url="http://${WP_DOMAIN}" --title="Wordpress Stage" --admin_user="${DB_USER}" --admin_password="${DB_PASS}" --admin_email="${WP_EMAIL}" | grep -ic "error"`
+    RESULT=`wp-cli core install --url="${WP_DOMAIN}" --title="Wordpress Stage" --admin_user="${DB_USER}" --admin_password="${DB_PASS}" --admin_email="${WP_EMAIL}" | grep -ic "error"`
     sleep 1
 done
+
+if [ -f "/db/dump.sql" ]; then
+    wp-cli db import /db/dump.sql
+    wp-cli db query "UPDATE wp_options SET option_value='${WP_DOMAIN}' WHERE option_name IN ('siteurl', 'home')"
+fi
 
 CONF="/**\n"
 CONF=${CONF}" * Handle SSL reverse proxy\n"
